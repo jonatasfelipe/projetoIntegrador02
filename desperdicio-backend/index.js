692d748d58e44c02ec8d7c1ce1987a855fa32959
Jônatas Felipe Santos Gonçalves <jonatas.felipe@hotmail.com.br>
2024-04-28 23:46:53 -0300

    swagger

--- desperdicio-backend/index.js
+++ desperdicio-backend/index.js
@@ -8,12 +8,14 @@ const db = require('./db');
 
 const cors = require("cors");
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 
 //middleware
 app.use(cors());
 app.use(bodyparser.json())
 //app.use('/api/donations', donationRoutes)
-app.use('/api/donations', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile, {customCssUrl: CSS_URL} ));
 
 //first make sure db connection is successful
 //then start the express server.
