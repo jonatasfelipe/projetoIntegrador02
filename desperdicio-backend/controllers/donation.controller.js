a96c4053c10a04632ac761982ca5308f6e2ec361
Jônatas Felipe Santos Gonçalves <jonatas.felipe@hotmail.com.br>
2024-04-29 00:46:29 -0300

    correção das urls

--- desperdicio-backend/controllers/donation.controller.js
+++ desperdicio-backend/controllers/donation.controller.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
 
 const service = require('../services/donation.service')
 
-//https://projeto-integrador02-backend.vercel.app/
+//https://projeto-integrador02-backend.vercel.app/api/donations/
 app.get('/', async (req, res) => {
     const donations = await service.getAllDonations()
     res.send(donations)
--- desperdicio-backend/index.js
+++ desperdicio-backend/index.js
@@ -8,23 +8,14 @@ const db = require('./db');
 
 const cors = require("cors");
 
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
-
-
 //middleware
 app.use(cors());
 app.use(bodyparser.json())
 //app.use('/api/donations', donationRoutes)
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile, {customCssUrl: CSS_URL} ));
+app.use('/api/donations', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-//first make sure db connection is successful
-//then start the express server.
-db.query("SELECT 1")
-    .then(() => {
-        console.log('db connection  succeeded.')
-        app.listen(3001,
-            () => console.log('server started at 3001'))
-    })
-    .catch(err => console.log('db connection failed. \n' + err))
+app.listen(3001,() => console.log('server started at 3001'));
 
 require('./controllers/donation.controller.js')(app);
+
+module.exports = app;
